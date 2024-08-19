fbddc15c856cd734bad681c79b43f1fbb0632871
İbrahim KAYA <ibkyey@gmail.com>
2024-08-19 23:55:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ from llama_index.prompts import PromptTemplate
 import os
 import matplotlib.pyplot as plt
 
-
 # API Anahtarı
 api_key = os.getenv("OPENAI_API_KEY")
 
@@ -51,7 +50,7 @@ if uploaded_file is not None:
         faiss.normalize_L2(query_embedding)
 
         # FAISS üzerinden en yakın sütunları bul
-        k = 3  # En yakın 2 sütun
+        k = 3  # En yakın 3 sütun
         distances, indices = index.search(query_embedding, k)
         relevant_columns = [df.columns[i] for i in indices[0]]
         
@@ -122,12 +121,13 @@ if uploaded_file is not None:
                 ]
             )
             qp.add_link("response_synthesis_prompt", "llm2")
-            fig, ax = plt.subplots()
+
             # Sorguyu çalıştır
             response = qp.run(query_str=query_str)
             st.write("Yanıt:")
             st.write(response.message.content)
 
-            if "plt" in response.message.content:
+            # Eğer çıktı matplotlib figürü içeriyorsa, bunu çizdir
+            if "plot" in response.message.content:
                 exec(response.message.content)
-                st.pyplot(fig=fig)
+                st.pyplot(fig=plt.gcf())
