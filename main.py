acacf79d6c08aa12e16b198aa74c09475cb2874d
İbrahim KAYA <88154235+ibkya@users.noreply.github.com>
2024-09-11 02:41:22 +0300

    test

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ import matplotlib.pyplot as plt
 api_key = os.getenv("OPENAI_API_KEY")
 
 # Streamlit başlat
-st.title("Verilerinizle Konuşun ve Görselleştirin!")
-st.write("CSV dosyanızı yükleyin ve sorgunuzu girin.")
+st.title("Chat with Excel and Visualize!")
+st.write("Upload csv and lets start!")
 
 #LLM seçimi(İstenilen LLM modeli implemente edilebilir. Şu anlık sunucu ücretsiz olduğu için sadece OpenAI desteklenmektedir.)
-model_type = st.selectbox("Kullanmak istediğiniz dil modelini seçin", ["OpenAI", "Llama-3.1:8B(Offline(Bellek sorunu))","Llama-3.1:70B(Offline(Bellek sorunu))"])
+model_type = st.selectbox("Chose your model", ["Llama-3.1:8B","Llama-3.1:70B(Offline(Bellek sorunu))"])
 llm = OpenAI(model="gpt-3.5-turbo")
 
 #CSV dosyasınızı yükleme kısmı
-uploaded_file = st.file_uploader("Bir CSV dosyası yükleyin", type=["csv"])
+uploaded_file = st.file_uploader("Upload a Excel File", type=["csv"])
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.write("Yüklenen veri:")
+    st.write("Uploaded File:")
     st.write(df.head())
 
     #SentenceTransformer modelini yükleme bu kodu çalıştırdığınızda hugging face üzerinden direkt indirmeye başlayacaktır.
@@ -43,7 +43,7 @@ if uploaded_file is not None:
     index.add(column_embeddings)  #Tüm sütun vektörlerini vektörize veritabanımıza ekliyoruz.
 
     #Kullanıcının arayüz üzerinden bir sorgu girebilmesi için alan açıyoruz.
-    query_str = st.text_input("Sorgunuzu girin", value="")
+    query_str = st.text_input("Type Here:", value="")
 
     if query_str:
         #Kullanıcıdan gelen sorguyu vektörleştirip ve normalize ediyoruz.
@@ -55,47 +55,49 @@ if uploaded_file is not None:
         distances, indices = index.search(query_embedding, k) #Vektörize veritabanında sorgumuzu gerçekleştiriyoruz.
         relevant_columns = [df.columns[i] for i in indices[0]] #Sütunları bir değişkene atayıp diğer adımda ekrana bastırıyoruz.
         
-        st.write("İlgili sütunlar:", relevant_columns)
+        st.write("Similar Columns", relevant_columns)
 
         #Sadece belirlenen sütunlar üzerinden bir sorgu üretebilmek adına sisteme sadece seçilen sütunları gönderiyoruz.
         if relevant_columns:
             selected_columns = relevant_columns  #En yakın sütunları veriyoruz.
-            st.write(f"Sorgu sadece şu sütunlar üzerinden gerçekleştirilecek: {selected_columns}")
+            st.write(f"Similar Columns: {selected_columns}")
             
             #Pandas sorgusu için talimatları dinamik olarak oluşturmak üzere bir prompt engineering yapıyoruz.
             instruction_str = (
-                f"Yalnızca {', '.join(selected_columns)} sütunlarını kullanarak sorguyu Pandas ile çalıştırılabilir Python koduna çevirin.\n"
-                "Eğer karmaşık işlemler gerekiyorsa, gruplama, toplama, birleştirme veya şekillendirme fonksiyonlarını kullanmayı düşünün.\n"
-                "Eğer grafik isteniyorsa matplotlib ile uygun grafiği oluşturmayı düşünün.\n"
-                "Eksik verileri uygun şekilde ele alın.\n"
-                "Kodu, `eval()` fonksiyonuyla çalıştırılabilecek bir Python ifadesiyle bitirin.\n"
-                "SADECE İFADEYİ YAZDIRIN.\n"
-                "İfadeyi tırnak içinde yazmayın.\n"
-                "Sadece Türkçe dilini kullanın.\n"
-            )
-            
+                    f"Translate the query into Python code that can be executed with Pandas, using only the columns: {', '.join(selected_columns)}.\n"
+                    "If complex operations are needed, consider using functions like grouping, aggregation, merging, or reshaping.\n"
+                    "If a plot is requested, consider creating the appropriate plot using matplotlib.\n"
+                    "Handle missing data appropriately.\n"
+                    "End the code with a Python expression that can be executed with the `eval()` function.\n"
+                    "PRINT ONLY THE EXPRESSION.\n"
+                    "Do not enclose the expression in quotes.\n"
+                    "Use only the English language.\n"
+                )
+
             pandas_prompt_str = (
-                "Python'da bir pandas dataframe ile çalışıyorsunuz.\n"
-                "Dataframe'in adı `df`.\n"
-                "Bu, `print(df.head())` çıktısının sonucudur:\n"
-                "{df_str}\n\n"
-                "Bu talimatları izleyin:\n"
-                "{instruction_str}\n"
-                "Sorgu: {query_str}\n\n"
-                "İfade:"
-            )
+                    "You are working with a pandas dataframe in Python.\n"
+                    "The name of the dataframe is `df`.\n"
+                    "This is the result of `print(df.head())`:\n"
+                    "{df_str}\n\n"
+                    "Follow these instructions:\n"
+                    "{instruction_str}\n"
+                    "Query: {query_str}\n\n"
+                    "Expression:"
+                )
 
             pandas_prompt = PromptTemplate(pandas_prompt_str).partial_format(
-                instruction_str=instruction_str, df_str=df[selected_columns].head(5)
-            )
+                    instruction_str=instruction_str, df_str=df[selected_columns].head(5)
+                )
+            
             pandas_output_parser = PandasInstructionParser(df[selected_columns])
             response_synthesis_prompt = PromptTemplate(
-                "Girdiğiniz soruya göre, sorgu sonuçlarından detaylı bir yanıt üretin.\n"
-                "Sorgu: {query_str}\n\n"
-                "Pandas Talimatları:\n{pandas_instructions}\n\n"
-                "Pandas Çıktısı: {pandas_output}\n\n"
-                "Yanıt: "
-            )
+                    "Generate a detailed response from the query results based on your input.\n"
+                    "Query: {query_str}\n\n"
+                    "Pandas Instructions:\n{pandas_instructions}\n\n"
+                    "Pandas Output: {pandas_output}\n\n"
+                    "Response: "
+                )
+
 
             #Genel işleyişin bir ilerleme mimarisini kurmak için QueryPipeline oluşturuyoruz.
             qp = QP(
@@ -125,7 +127,7 @@ if uploaded_file is not None:
             fig, ax = plt.subplots()
             #Gelen sorguyu oluşturduğumuz işleyiş mimarisine aktarıp sistemi çalıştırıyoruz.
             response = qp.run(query_str=query_str)
-            st.write("Yanıt:")
+            st.write("Response:")
             st.write(response.message.content)
 
 
